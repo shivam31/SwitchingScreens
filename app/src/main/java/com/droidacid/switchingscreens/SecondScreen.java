5f3b36b40e7b8a409e12e067460f22a5cdf5287e
Shivam Chopra <shivamdev31@gmail.com>
2015-01-27 02:37:18 +0530

    Initial commit

---
+++ app/src/main/java/com/droidacid/switchingscreens/SecondScreen.java
@@ -0,0 +1,15 @@
+package com.droidacid.switchingscreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shivam.chopra on 27-01-2015.
+ */
+public class SecondScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
